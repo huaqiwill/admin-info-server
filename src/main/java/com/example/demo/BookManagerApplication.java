8712f822da8773081d10751b8aea18167490ef19
huaqiwill <3173484026@qq.com>
2024-06-07 00:55:59 +0800

    首次提交

---
+++ src/main/java/com/example/demo/BookManagerApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BookManagerApplication.class, args);
+        System.out.println("系统启动成");
+    }
+
+}
---
+++ src/main/java/com/example/demo/mapper/BookMapper.java
@@ -0,0 +1,10 @@
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.Book;
+import org.apache.ibatis.annotations.Insert;
+
+
+public interface BookMapper extends BaseMapper<Book> {
+
+}
